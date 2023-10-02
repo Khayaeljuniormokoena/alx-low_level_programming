bbcd05761e879f18512218636e9a2caf98cd89d5
Khayaeljuniormokoena <khayamokoena82@gmail.com>
2023-10-02 01:24:57 -0700

    update

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads and prints a text file to the POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes_read, bytes_written;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, 0_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILEND, buf, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (bytes_written);
+}
