30b1dddbc5cab8b810ed7f0678b8be2b6c8fc9cb
Khayaeljuniormokoena <khayamokoena82@gmail.com>
2023-11-13 10:29:47 -0800

    update

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - div 2 int
+ * @a: first int
+ * @b: second int
+ * Return: div result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+/**
+ * add - add 2 int
+ * @a: first int
+ * @b: ssecond int
+ * Return: add
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+}
+
+/**
+ * sub - sub 2 int
+ * @a: first int
+ * @b: ssecond int
+ * Return: sub
+ */
+
+int sub(int a, int b)
+{
+
+        return (a - b);
+}
+
+/**
+ * mul - mul 2 int
+ * @a: first int
+ * @b: ssecond int
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+
+        return (a * b);
+}
+
+/**
+ * mod - mod 2 int
+ * @a: first int
+ * @b: ssecond int
+ * Return: mod
+ */
+
+int mod(int a, int b)
+{
+
+        return (a % b);
+}
